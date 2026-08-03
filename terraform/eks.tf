39e8813315877d55e0b5ba9b5a80f95fe7bab35b
Yash <yashkriti24@gmail.com>
2026-08-03 17:28:25 +0000

    feat(k8s): updated kubernetes manifests with ECR URIs and RDS endpoint configs

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -11,24 +11,11 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  enable_irsa = true
+  enable_irsa                              = true
   enable_cluster_creator_admin_permissions = true
 
-  access_entries = {
-    terraform_user = {
-      principal_arn = "arn:aws:iam::277328279179:user/terraform-user"
-
-      policy_associations = {
-        admin = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-
-          access_scope = {
-            type = "cluster"
-          }
-        }
-      }
-    }
-  }
+ 
+  
 
   cluster_compute_config = {
     enabled    = true
